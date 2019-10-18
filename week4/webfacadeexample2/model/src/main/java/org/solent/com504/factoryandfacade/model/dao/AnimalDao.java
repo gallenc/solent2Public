a4b1ed2d919ec488d89e64f5a7c4168f17dcf4b5
Craig Gallen <Craig Gallen>
2019-10-18 15:24:18 +0100

    updated javadoc

--- week4/webfacadeexample2/model/src/main/java/org/solent/com504/factoryandfacade/model/dao/AnimalDao.java
+++ week4/webfacadeexample2/model/src/main/java/org/solent/com504/factoryandfacade/model/dao/AnimalDao.java
@@ -7,24 +7,25 @@ import org.solent.com504.factoryandfacade.model.dto.AnimalType;
 public interface AnimalDao {
 
     /**
-     *
+     * Returns an animal which has a corresponding id 
      * @param id
-     * @return
+     * @return Animal with animal.id = id OR returns null if animal not found.
      */
     public Animal retrieve(long id);
 
     /**
-     * If animal has no id saves animal in the database with a new id If animal has an id tries to update animal but if animal with id is not found returns null
+     * If animal has no id saves animal in the database with a new id 
+     * If animal has an id, tries to update animal but if animal with id is not found returns null
      * and does nothing (animal must have no id field set if creating new animal)
      *
      * @param animal
-     * @return animal which has been creates in data base or updates in database
+     * @return animal which has been created in the database or updates the existing animal in the  database
      *
      */
     public Animal updateOrSave(Animal animal);
 
     /**
-     * Deletes an item with an id matching id
+     * Deletes an item with an id matching animal.id
      *
      * @param id
      * @return true if item with id is found and deleted. False if not found
@@ -32,22 +33,27 @@ public interface AnimalDao {
     public boolean delete(long id);
 
     /**
-     * retrieves a list of animals which exactly match the fields in the animal template Fields are 'anded' Null fields are ignored
+     * Uses an Animal object as a template to retrieve a matching animal in the database.
+     * Retrieves a list of animals which exactly match the populated fields in the animalTemplate. 
+     * Null fields in animalTemplate are ignored.
+     * Matching fields are 'ANDed' such that all populated fields in the returned animal must match the corresponding fields in the template.
      *
      * @param animalTemplate
-     * @return List of animals or empty list
+     * @return List of animals or empty list if no matches
      */
     public List<Animal> retrieve(Animal animalTemplate);
 
     /**
-     * retrieves all animals in list
+     * Retrieves all animals which have been persisted in a list.
      *
-     * @return
+     * @return Lost of all animals in DAO
      */
     public List<Animal> retrieveAll();
 
     /**
-     * creates a new animal of animal type. Animal does not have an id and is not persisted when created.
+     * Creates a new animal of animal type. 
+     * At this point the returned animal is not persisted and does not have an id.
+     * It should be persisted using updateOrSave
      *
      * @param animalType type of animal to create
      * @return animal of animalType but with no id set
