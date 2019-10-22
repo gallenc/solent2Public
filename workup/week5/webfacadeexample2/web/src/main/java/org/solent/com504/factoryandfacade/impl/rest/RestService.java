8e4df2e03c5219961f8e63bdbfa5d3e01a2a3d7e
Craig Gallen <Craig Gallen>
2019-10-22 11:23:26 +0100

    log4j2 workubg

--- workup/week5/webfacadeexample2/web/src/main/java/org/solent/com504/factoryandfacade/impl/rest/RestService.java
+++ workup/week5/webfacadeexample2/web/src/main/java/org/solent/com504/factoryandfacade/impl/rest/RestService.java
@@ -25,21 +25,42 @@ import org.solent.com504.factoryandfacade.model.dto.Animal;
 import org.solent.com504.factoryandfacade.model.dto.ReplyMessage;
 import org.solent.com504.factoryandfacade.model.service.FarmFacade;
 
-
+/**
+ * To make the ReST interface easier to program. All of the replies are
+ * contained in ReplyMessage classes but only the fields indicated are populated
+ * with each reply. All replies will contain a code and a debug message.
+ * Possible replies are: List<String> replyMessage.getStringList() AnimalList
+ * replyMessage.getAnimalList() int replyMessage.getCode()
+ * replyMessage.getDebugMessage(); * @author cgallen
+ */
 @Path("/farmService")
 public class RestService {
-    // SETS UP LOGGING
-    final static Logger LOG = LogManager.getLogger(WebObjectFactory.class);
-    
 
-    // http://localhost:8084/basicfacadeweb/rest/farmService/
+    // SETS UP LOGGING 
+    // note that log name will be org.solent.com504.factoryandfacade.impl.rest.RestService
+    final static Logger LOG = LogManager.getLogger(RestService.class);
+
+    /**
+     * this is a very simple rest test message which only returns a string
+     *
+     * http://localhost:8084/basicfacadeweb/rest/farmService/
+     *
+     * @return String simple message
+     */
     @GET
     public String message() {
+
         LOG.debug("farmService called");
         return "Hello, rest!";
     }
 
-    //http://localhost:8084/basicfacadeweb/rest/farmService/
+    /**
+     * Get all animals on the farm
+     *
+     * http://localhost:8084/basicfacadeweb/rest/farmService/
+     *
+     * @return list of all Animals in List<String> replyMessage.getStringList()
+     */
     @GET
     @Path("/getAllAnimals")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
@@ -48,14 +69,14 @@ public class RestService {
 
             FarmFacade serviceFacade = WebObjectFactory.getServiceFacade();
             ReplyMessage replyMessage = new ReplyMessage();
+            LOG.debug("/getAllAnimals called");
 
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 
-            //LOG.debug("/retrievematching entityTemplate: " + entityTemplate);
             //replyMessage.setCode(Response.Status.OK.getStatusCode());
             //return Response.status(Response.Status.OK).entity(replyMessage).build();
         } catch (Exception ex) {
-            //LOG.error("error calling /retrievematching ", ex);
+            LOG.error("error calling /getAllAnimals ", ex);
             ReplyMessage replyMessage = new ReplyMessage();
             replyMessage.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
             replyMessage.setDebugMessage("error calling /getAllAnimals " + ex.getMessage());
@@ -63,27 +84,41 @@ public class RestService {
         }
     }
 
-    // http://localhost:8084/basicfacadeweb/rest/farmService/addAnimal
+    /**
+     * Creates an Animal of a given AnimalType with a name and adds the animal
+     * to the list of animals stored by this farm. Note only one animal can be
+     * added with the same name
+     *
+     * http://localhost:8084/basicfacadeweb/rest/farmService/addAnimal
+     *
+     * @param animalType type of animal ( which must have been created by the
+     * AnimalTypeDao)
+     * @param name name to give this animal - does not have to be unique
+     * @return an animal of a given type with the supplied name which has been
+     * stored. Animal will also have been given an id.
+     * @ returns error code if animal name is duplicated. All animal names must
+     * be unique. AnimalList replyMessage.getAnimalList().get(0) contains
+     * returned animal
+     */
     @POST
     @Path("/addAnimal")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response addAnimal(@QueryParam("animalType") String animalType, @QueryParam("animalName") String animalName) {
         try {
-            
-            //LOG.debug("/retrievematching entityTemplate: " + entityTemplate);
-            
+            LOG.debug("/addAnimal called animalType=" + animalType + "animalName=" + animalName);
+
             FarmFacade serviceFacade = WebObjectFactory.getServiceFacade();
             ReplyMessage replyMessage = new ReplyMessage();
 
             Animal addedAnimal = serviceFacade.addAnimal(animalType, animalName);
             replyMessage.getAnimalList().getAnimals().add(addedAnimal);
-            
+
             replyMessage.setCode(Response.Status.OK.getStatusCode());
-           
+
             return Response.status(Response.Status.OK).entity(replyMessage).build();
 
         } catch (Exception ex) {
-            //LOG.error("error calling /retrievematching ", ex);
+            LOG.error("error calling /addAnimal ", ex);
             ReplyMessage replyMessage = new ReplyMessage();
             replyMessage.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
             replyMessage.setDebugMessage("error calling /addAnimal " + ex.getMessage());
@@ -91,12 +126,22 @@ public class RestService {
         }
     }
 
-    // http://localhost:8084/basicfacadeweb/rest/farmService/getAnimalsOfType
+    /**
+     * returns all animals in farm of a given type
+     *
+     * http://localhost:8084/basicfacadeweb/rest/farmService/getAnimalsOfType
+     *
+     * @param animalType type name for the animal type
+     * @return returns a list of all animals of a given type AnimalList
+     * replyMessage.getAnimalList() contains returned animals
+     */
     @GET
     @Path("/getAnimalsOfType")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getAnimalsOfType(@QueryParam("animalType") String animalType) {
         try {
+            LOG.debug("/getAnimalsOfType called animalType=" + animalType);
+
             FarmFacade serviceFacade = WebObjectFactory.getServiceFacade();
             ReplyMessage replyMessage = new ReplyMessage();
 
@@ -106,7 +151,7 @@ public class RestService {
             //replyMessage.setCode(Response.Status.OK.getStatusCode());
             //return Response.status(Response.Status.OK).entity(replyMessage).build();
         } catch (Exception ex) {
-            //LOG.error("error calling /retrievematching ", ex);
+            LOG.error("error calling /getAnimalsOfType ", ex);
             ReplyMessage replyMessage = new ReplyMessage();
             replyMessage.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
             replyMessage.setDebugMessage("error calling /getAnimalsOfType " + ex.getMessage());
@@ -114,12 +159,21 @@ public class RestService {
         }
     }
 
-    // http://localhost:8084/basicfacadeweb/rest/farmService/getAnimal
+    /**
+     * returns the first animal in the list which has a given name Note that
+     * names should be unique so only one animal of this name in list.
+     *
+     * http://localhost:8084/basicfacadeweb/rest/farmService/getAnimal *
+     *
+     * @param animalName name of animal to retrieve
+     * @return ReplyMesssage AnimalList replyMessage.getAnimalList()
+     */
     @GET
     @Path("/getAnimal")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getAnimal(@QueryParam("animalName") String animalName) {
         try {
+            LOG.debug("/getAnimal called animalName=" + animalName);
             FarmFacade serviceFacade = WebObjectFactory.getServiceFacade();
             ReplyMessage replyMessage = new ReplyMessage();
 
@@ -129,7 +183,7 @@ public class RestService {
             //replyMessage.setCode(Response.Status.OK.getStatusCode());
             //return Response.status(Response.Status.OK).entity(replyMessage).build();
         } catch (Exception ex) {
-            //LOG.error("error calling /retrievematching ", ex);
+            LOG.error("error calling /getAnimal ", ex);
             ReplyMessage replyMessage = new ReplyMessage();
             replyMessage.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
             replyMessage.setDebugMessage("error calling /getAnimal " + ex.getMessage());
@@ -137,22 +191,33 @@ public class RestService {
         }
     }
 
-    // http://localhost:8084/basicfacadeweb/rest/farmService/removeAnimal POST not GET
+    /**
+     * remove the first animal in the list which has a given name Note HTTP post
+     * must be used
+     * http://localhost:8084/basicfacadeweb/rest/farmService/removeAnimal POST
+     * not GET
+     *
+     * @param animalName
+     * @return ReplyMessage OK if deleted animal
+     */
     @POST
     @Path("/removeAnimal")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response removeAnimal(@QueryParam("animalName") String name) {
+    public Response removeAnimal(@QueryParam("animalName") String animalName) {
         try {
+
+            LOG.debug("/removeAnimal called animalName=" + animalName);
+
             FarmFacade serviceFacade = WebObjectFactory.getServiceFacade();
             ReplyMessage replyMessage = new ReplyMessage();
 
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            throw new UnsupportedOperationException("Not supported yet.");
 
             //LOG.debug("/retrievematching entityTemplate: " + entityTemplate);
             //replyMessage.setCode(Response.Status.OK.getStatusCode());
             //return Response.status(Response.Status.OK).entity(replyMessage).build();
         } catch (Exception ex) {
-            //LOG.error("error calling /retrievematching ", ex);
+            LOG.error("error calling /removeAnimal ", ex);
             ReplyMessage replyMessage = new ReplyMessage();
             replyMessage.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
             replyMessage.setDebugMessage("error calling /removeAnimal " + ex.getMessage());
@@ -160,12 +225,20 @@ public class RestService {
         }
     }
 
-    // http://localhost:8084/basicfacadeweb/rest/farmService/getSupportedAnimalTypes
+    /**
+     * returns a list of strings describing the supported animal types
+     *
+     * http://localhost:8084/basicfacadeweb/rest/farmService/getSupportedAnimalTypes
+     *
+     * @return ReplyMessage List<String> replyMessage.getStringList()
+     */
     @GET
     @Path("/getSupportedAnimalTypes")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getSupportedAnimalTypes() {
         try {
+            LOG.debug("/getSupportedAnimalTypes called");
+
             FarmFacade serviceFacade = WebObjectFactory.getServiceFacade();
             ReplyMessage replyMessage = new ReplyMessage();
 
@@ -177,7 +250,7 @@ public class RestService {
             replyMessage.setCode(Response.Status.OK.getStatusCode());
             return Response.status(Response.Status.OK).entity(replyMessage).build();
         } catch (Exception ex) {
-            //LOG.error("error calling /retrievematching ", ex);
+            LOG.error("error calling /getSupportedAnimalTypes ", ex);
             ReplyMessage replyMessage = new ReplyMessage();
             replyMessage.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
             replyMessage.setDebugMessage("error calling /getSupportedAnimalTypes " + ex.getMessage());
