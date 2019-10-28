ba89a32c9c8540c2eb721f1b0143b5856d74e68d
Craig Gallen <cgallen@opennms.org>
2019-10-28 22:37:18 +0000

    working dao jdbc

--- workup/week6/webfacadeexample2/dao-jdbc/src/main/java/org/solent/com504/factoryandfacade/impl/dao/jdbc/AnimalDaoJdbcImpl.java
+++ workup/week6/webfacadeexample2/dao-jdbc/src/main/java/org/solent/com504/factoryandfacade/impl/dao/jdbc/AnimalDaoJdbcImpl.java
@@ -9,12 +9,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import org.solent.com504.factoryandfacade.model.dao.AnimalDao;
 import org.solent.com504.factoryandfacade.model.dto.Animal;
 import org.solent.com504.factoryandfacade.model.dto.AnimalType;
@@ -26,20 +23,20 @@ import org.apache.logging.log4j.Logger;
  * @author cgallen
  */
 public class AnimalDaoJdbcImpl implements AnimalDao {
-    
+
+    //FOR TUTORIAL SEE https://www.roseindia.net/tutorial/java/jdbc/dataaccessobjectdesignpattern.html  Data Access object (DAO) Design Pattern
     final static Logger LOG = LogManager.getLogger(AnimalDaoJdbcImpl.class);
     
     private ConnectionFactory connectionFactory;
     
     public AnimalDaoJdbcImpl(ConnectionFactory cf) {
+        LOG.debug("AnimalDaoJdbcImpl trying to create new database table");
         this.connectionFactory = cf;
 
         // create database
         PreparedStatement ptmt = null;
         ResultSet resultSet = null;
         Connection connection = null;
-        
-        Animal animal = null;
         try {
             connection = connectionFactory.getConnection();
             
@@ -52,6 +49,7 @@ public class AnimalDaoJdbcImpl implements AnimalDao {
             // see https://stackoverflow.com/questions/5866154/how-to-create-table-if-it-doesnt-exist-using-derby-db
             LOG.error("table creation exception (may happen if duplicate table exists)", e);
         } finally {
+            // this releases all of the resources - must be in finally
             try {
                 if (resultSet != null) {
                     resultSet.close();
@@ -78,34 +76,37 @@ public class AnimalDaoJdbcImpl implements AnimalDao {
         Connection connection = null;
         Animal animal = null;
         try {
+            LOG.debug("retrieve called for id=  " + id);
             connection = connectionFactory.getConnection();
             
-            String queryString = "INSERT INTO ANIMAL (RollNo, Name, Course, Address) VALUES(?,?,?,?)";
-            
-            resultSet = ptmt.executeQuery();
-			while (resultSet.next()) {
-				System.out.println("Roll No " + resultSet.getInt("RollNo")
-						+ ", Name " + resultSet.getString("Name") + ", Course "
-						+ resultSet.getString("Course") + ", Address "
-						+ resultSet.getString("Address"));
-			}
-            
-            animal = new Animal();
-            animal.setAddress(queryString);
-            AnimalType animalType = new AnimalType();
-            animal.setAnimalType(animalType);
-            animal.setId(id);
-            String name = null;
-            animal.setName(name);
+            String queryString = "SELECT ID, NAME, ADDRESS, SOUND, TYPE FROM ANIMAL WHERE ID=?";
             ptmt = connection.prepareStatement(queryString);
             ptmt.setLong(1, id);
             
-            ptmt.executeUpdate();
+            resultSet = ptmt.executeQuery();
+            
+            if (resultSet.next()) {
+                animal = new Animal();
+                animal.setId(resultSet.getLong("ID"));
+                animal.setName(resultSet.getString("NAME"));
+                animal.setAddress(resultSet.getString("ADDRESS"));
+                
+                AnimalType animalType = new AnimalType();
+                animal.setAnimalType(animalType);
+                animalType.setSound(resultSet.getString("SOUND"));
+                animalType.setType(resultSet.getString("TYPE"));
+                LOG.debug("animal retrieved for id=  " + id + " " + animal);
+                return animal;
+            } else {
+                LOG.debug("no animal retrieved for id=  " + id);
+                return animal;
+            }
             
         } catch (Exception e) {
             LOG.error("retrieve exception", e);
             throw new RuntimeException("retrieve exception", e);
         } finally {
+            // this releases all of the resources - must be in finally
             try {
                 if (resultSet != null) {
                     resultSet.close();
@@ -124,108 +125,309 @@ public class AnimalDaoJdbcImpl implements AnimalDao {
             return animal;
         }
     }
-
-//    @Override
-//    public Animal updateOrSave(Animal animal) {
-//        connectionFactory.getTransaction().begin();
-//        connectionFactory.persist(animal);  // NOTE merge(animal) differnt semantics
-//        // connectionFactory.flush() could be used
-//        connectionFactory.getTransaction().commit();
-//        return animal;
-//    }
-//
-//    @Override
-//    public boolean delete(long id) {
-//        connectionFactory.getTransaction().begin();
-//        Query q = connectionFactory.createQuery("DELETE FROM Animal a WHERE a.id=:id");
-//        q.setParameter("id", id);
-//        q.executeUpdate();
-//        connectionFactory.getTransaction().commit();
-//
-//        return true;
-//    }
-//
-//    @Override
-//    public List<Animal> retrieve(Animal animalTemplate) {
-//
-//        Map<String, String> paramMap = new HashMap<String, String>();
-//
-//        String queryString = "select a from Animal a WHERE TRUE=TRUE  "; // WHERE TRUE=TRUE masn WHERE always has a predicate ";
-//        if (animalTemplate.getName() != null) {
-//            queryString = queryString + "AND a.name LIKE :name "; //':name' ";
-//            paramMap.put("name", animalTemplate.getName());
-//        }
-//        if (animalTemplate.getAddress() != null) {
-//            queryString = queryString + "AND a.address LIKE :address ";
-//            paramMap.put("address", animalTemplate.getAddress());
-//        }
-//        if (animalTemplate.getAnimalType() != null && animalTemplate.getAnimalType().getType() != null) {
-//            queryString = queryString + "AND a.animalType.type LIKE :type ";
-//            paramMap.put("type", animalTemplate.getAnimalType().getType());
-//        }
-//
-//        // prevents running this section if not printing debug log
-//        if (LOG.isDebugEnabled()) {
-//            LOG.debug("queryString string built: " + queryString + "using parameters: ");
-//            for (String key : paramMap.keySet()) {
-//                LOG.debug("key: " + key + " value:" + paramMap.get(key));
-//            }
-//        }
-//
-//        TypedQuery<Animal> query = connectionFactory.createQuery(queryString, Animal.class);
-//        for (String key : paramMap.keySet()) {
-//            query.setParameter(key, paramMap.get(key));
-//        }
-//
-//        List<Animal> animalList = query.getResultList();
-//        return animalList;
-//    }
-//
-//    @Override
-//    public List<Animal> retrieveAll() {
-//        TypedQuery<Animal> q = connectionFactory.createQuery("SELECT a FROM Animal a", Animal.class);
-//        List<Animal> animalList = q.getResultList();
-//        return animalList;
-//    }
-//
-//    @Override
-//    public void deleteAll() {
-//        connectionFactory.getTransaction().begin();
-//        connectionFactory.createQuery("DELETE FROM Animal ").executeUpdate();
-//        connectionFactory.getTransaction().commit();
-//    }
-    // no need to synchronize - same as simple dao
-    @Override
-    public Animal create(AnimalType animalType
-    ) {
-        Animal animal = new Animal();
-        animal.setAnimalType(animalType);
-        return animal;
-    }
     
     @Override
     public Animal updateOrSave(Animal animal) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ptmt = null;
+        ResultSet resultSet = null;
+        Connection connection = null;
+        
+        try {
+            LOG.debug("updateOrSave animal=  " + animal);
+            connection = connectionFactory.getConnection();
+            String queryString = null;
+            
+            if (animal.getId() == null) {
+                queryString = "INSERT INTO ANIMAL (NAME, ADDRESS, SOUND, TYPE) VALUES(?,?,?,?)";
+            } else {
+                queryString = "UPDATE ANIMAL SET NAME=?, ADDRESS=?, SOUND=?, TYPE=? WHERE ID=?";
+            }
+            ptmt = connection.prepareStatement(queryString, Statement.RETURN_GENERATED_KEYS);
+            ptmt.setString(1, animal.getName());
+            ptmt.setString(2, animal.getAddress());
+            ptmt.setString(3, animal.getAnimalType().getSound());
+            ptmt.setString(4, animal.getAnimalType().getType());
+            
+            if (animal.getId() != null) {
+                // updating animal with existing id
+                ptmt.setLong(5, animal.getId());
+                int rowAffected = ptmt.executeUpdate();
+                if (rowAffected == 1) {
+                    LOG.debug("updated animal=  " + animal);
+                    return animal;
+                } else {
+                    LOG.debug("failed to update animal=  " + animal);
+                    return null;
+                }
+                
+            } else {
+                // creating new animal
+                int rowAffected = ptmt.executeUpdate();
+                if (rowAffected == 1) {
+                    // get candidate id
+                    ResultSet rs = ptmt.getGeneratedKeys();
+                    if (rs.next()) {
+                        Long candidateId = rs.getLong(1);
+                        animal.setId(candidateId);
+                        LOG.debug("inserted animal=  " + animal);
+                        return animal;
+                    }
+                }
+                
+            }
+            return null;
+        } catch (Exception e) {
+            LOG.error("updateOrSave exception", e);
+            throw new RuntimeException("updateOrSave exception", e);
+        } finally {
+            // this releases all of the resources - must be in finally
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (ptmt != null) {
+                    ptmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOG.error("finally exception", e);
+            } catch (Exception e) {
+                LOG.error("rfinally exception", e);
+            }
+            
+        }
+        
     }
     
     @Override
     public boolean delete(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ptmt = null;
+        ResultSet resultSet = null;
+        Connection connection = null;
+        Animal animal = null;
+        try {
+            LOG.debug("delete called for id=  " + id);
+            connection = connectionFactory.getConnection();
+            
+            String queryString = "DELETE FROM ANIMAL WHERE ID=?";
+            ptmt = connection.prepareStatement(queryString);
+            ptmt.setLong(1, id);
+            
+            long rowAffected = ptmt.executeUpdate();
+            if (rowAffected == 0) {
+                LOG.debug("no rows deleted");
+                return false;
+            }
+            LOG.debug(rowAffected + " rows deleted");
+            return true;
+            
+        } catch (Exception e) {
+            LOG.error("retrieve exception", e);
+            throw new RuntimeException("retrieve exception", e);
+        } finally {
+            // this releases all of the resources - must be in finally
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (ptmt != null) {
+                    ptmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOG.error("finally exception", e);
+            } catch (Exception e) {
+                LOG.error("rfinally exception", e);
+            }
+            
+        }
     }
     
     @Override
     public List<Animal> retrieve(Animal animalTemplate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ptmt = null;
+        ResultSet resultSet = null;
+        Connection connection = null;
+        List<Animal> animalList = new ArrayList();
+        Animal animal = null;
+        try {
+            LOG.debug("retrieve called animalTemplate=" + animalTemplate);
+            connection = connectionFactory.getConnection();
+            
+            List<String> valueList = new ArrayList<String>();
+            
+            String queryString = "SELECT ID, NAME, ADDRESS, SOUND, TYPE FROM ANIMAL WHERE TRUE=TRUE ";
+            // WHERE TRUE=TRUE means WHERE always has a predicate ";
+
+            if (animalTemplate.getName() != null) {
+                queryString = queryString + "AND NAME LIKE ? "; //':name' ";
+                valueList.add(animalTemplate.getName());
+            }
+            if (animalTemplate.getAddress() != null) {
+                queryString = queryString + "AND ADDRESS LIKE ?  ";
+                valueList.add(animalTemplate.getAddress());
+            }
+            if (animalTemplate.getAnimalType() != null && animalTemplate.getAnimalType().getType() != null) {
+                queryString = queryString + "AND TYPE LIKE ? ";
+                valueList.add(animalTemplate.getAnimalType().getType());
+            }
+
+            LOG.debug("queryString string built: " + queryString + "using parameters: ");
+            ptmt = connection.prepareStatement(queryString);
+            for (int index = 1; index <= valueList.size(); index++) {
+                LOG.debug(" index:"+(index-1)+"   value:" + valueList.get(index-1));
+                ptmt.setString(index, valueList.get(index-1));
+            }
+            
+            resultSet = ptmt.executeQuery();
+            
+            while (resultSet.next()) {
+                animal = new Animal();
+                animal.setId(resultSet.getLong("ID"));
+                animal.setName(resultSet.getString("NAME"));
+                animal.setAddress(resultSet.getString("ADDRESS"));
+                
+                AnimalType animalType = new AnimalType();
+                animal.setAnimalType(animalType);
+                animalType.setType(resultSet.getString("TYPE"));
+                animalType.setSound(resultSet.getString("SOUND"));
+                LOG.debug("animal retrieved " + animal);
+                animalList.add(animal);
+            }
+            if (animalList.isEmpty()) {
+                LOG.debug("retrieve - no animals retreived ");
+            }
+            return animalList;
+            
+        } catch (Exception e) {
+            LOG.error("retrieve exception", e);
+            throw new RuntimeException("retrieve exception", e);
+        } finally {
+            // this releases all of the resources - must be in finally
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (ptmt != null) {
+                    ptmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOG.error("finally exception", e);
+            } catch (Exception e) {
+                LOG.error("finally exception", e);
+            }
+        }
+        
     }
     
     @Override
     public List<Animal> retrieveAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ptmt = null;
+        ResultSet resultSet = null;
+        Connection connection = null;
+        List<Animal> animalList = new ArrayList();
+        Animal animal = null;
+        try {
+            LOG.debug("retrieveAll() called ");
+            connection = connectionFactory.getConnection();
+            
+            String queryString = "SELECT ID, NAME, ADDRESS, SOUND, TYPE FROM ANIMAL ";
+            ptmt = connection.prepareStatement(queryString);
+            
+            resultSet = ptmt.executeQuery();
+            
+            while (resultSet.next()) {
+                animal = new Animal();
+                animal.setId(resultSet.getLong("ID"));
+                animal.setName(resultSet.getString("NAME"));
+                animal.setAddress(resultSet.getString("ADDRESS"));
+                
+                AnimalType animalType = new AnimalType();
+                animal.setAnimalType(animalType);
+                animalType.setSound(resultSet.getString("SOUND"));
+                animalType.setType(resultSet.getString("TYPE"));
+                LOG.debug("animal retrieved " + animal);
+                animalList.add(animal);
+            }
+            if (animalList.isEmpty()) {
+                LOG.debug("retrieveAll() - no animals retreived ");
+            }
+            return animalList;
+            
+        } catch (Exception e) {
+            LOG.error("retrieve exception", e);
+            throw new RuntimeException("retrieve exception", e);
+        } finally {
+            // this releases all of the resources - must be in finally
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (ptmt != null) {
+                    ptmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOG.error("finally exception", e);
+            } catch (Exception e) {
+                LOG.error("finally exception", e);
+            }
+        }
     }
     
     @Override
     public void deleteAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ptmt = null;
+        ResultSet resultSet = null;
+        Connection connection = null;
+        try {
+            LOG.debug("deleteAll called");
+            connection = connectionFactory.getConnection();
+            
+            String queryString = "DELETE FROM ANIMAL";
+            
+            ptmt = connection.prepareStatement(queryString);
+            ptmt.execute();
+            
+        } catch (Exception e) {
+            
+            LOG.error("deleteAll exception", e);
+        } finally {
+            // this releases all of the resources - must be in finally
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (ptmt != null) {
+                    ptmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOG.error("finally exception", e);
+            } catch (Exception e) {
+                LOG.error("finally exception", e);
+            }
+        }
+        
+    }
+
+    // no need to synchronize - same as simple dao
+    @Override
+    public Animal create(AnimalType animalType) {
+        Animal animal = new Animal();
+        animal.setAnimalType(animalType);
+        return animal;
     }
     
 }
