d024151be3ebc0811a0b3d552c4376f3d2b7a189
Craig Gallen <Craig Gallen>
2019-11-22 09:01:50 +0000

    starting spring data2

--- workup/springDataExample1/dao-springdata-jpa/src/test/java/org/solent/com504/project/impl/dao/jpa/test/AppointmentDAOTest.java
+++ workup/springDataExample1/dao-springdata-jpa/src/test/java/org/solent/com504/project/impl/dao/jpa/test/AppointmentDAOTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.solent.com504.project.impl.dao.jpa.DAOFactoryJPAImpl;
 import org.solent.com504.project.model.dao.AppointmentDAO;
-
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -22,27 +22,28 @@ public class AppointmentDAOTest {
 
     final static Logger LOG = LogManager.getLogger(AppointmentDAOTest.class);
 
-    private AppointmentDAO appointmentDao = null;
-
-    private DAOFactoryJPAImpl daoFactory = new DAOFactoryJPAImpl();
-
-    @Before
-    public void before() {
-        appointmentDao = daoFactory.getAppointmentDAO();
-        assertNotNull(appointmentDao);
-    }
-
+//    private AppointmentDAO appointmentDao = null;
+//
+//    private DAOFactoryJPAImpl daoFactory = new DAOFactoryJPAImpl();
+//
+//    @Before
+//    public void before() {
+//        appointmentDao = daoFactory.getAppointmentDAO();
+//        assertNotNull(appointmentDao);
+//    }
+//
+//    @Test
+//    public void createAppointmentDAOTest() {
+//        LOG.debug("start of createAppointmentDAOTest(");
+//        // this test simply runs the before method
+//        LOG.debug("end of createAppointmentDAOTest(");
+//    }
+//    
     @Test
-    public void createAppointmentDAOTest() {
-        LOG.debug("start of createAppointmentDAOTest(");
-        // this test simply runs the before method
-        LOG.debug("end of createAppointmentDAOTest(");
+    public void test1() {
+        LOG.debug("start of test1");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/spring.xml");
+        LOG.debug("end of test1");
     }
-    
-    
-    
-    
-    
-    
-    
+
 }
--- workup/springDataExample1/dao-springdata-jpa/src/test/java/org/solent/com504/project/impl/dao/jpa/test/PersonDAOTest.java
+++ workup/springDataExample1/dao-springdata-jpa/src/test/java/org/solent/com504/project/impl/dao/jpa/test/PersonDAOTest.java
@@ -24,114 +24,114 @@ public class PersonDAOTest {
 
     final static Logger LOG = LogManager.getLogger(PersonDAOTest.class);
 
-    private PersonDAO personDao = null;
-
-    private DAOFactoryJPAImpl daoFactory = new DAOFactoryJPAImpl();
-
-    @Before
-    public void before() {
-        personDao = daoFactory.getPersonDAO();
-        assertNotNull(personDao);
-    }
-
-    // initialises database for each test
-    private void init() {
-        // delete all in database
-        personDao.deleteAll();
-        // add 5 patients
-        for (int i = 1; i < 6; i++) {
-            Person p = new Person();
-            p.setAddress("address_" + i);
-            p.setFirstName("firstName_" + i);
-            p.setSecondName("secondName_" + i);
-            p.setRole(Role.PATIENT);
-            personDao.save(p);
-        }
-    }
-
-    @Test
-    public void createPersonDAOTest() {
-        LOG.debug("start of createPersonDAOTest");
-        // this test simply runs the before method
-        LOG.debug("end of createPersonDAOTest");
-    }
-
-    @Test
-    public void findByIdTest() {
-        LOG.debug("start of findByIdTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end of findByIdTest()");
-    }
-
-    @Test
-    public void saveTest() {
-        LOG.debug("start of saveTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end of saveTest()");
-    }
-
-    @Test
-    public void findAllTest() {
-        LOG.debug("start of findAllTest()");
-
-        init();
-        List<Person> personList = personDao.findAll();
-        assertNotNull(personList);
-        
-        // init should insert 5 people
-        assertEquals(5, personList.size());
-
-        // print out result
-        String msg = "";
-        for (Person person : personList) {
-            msg = msg +"\n   " +  person.toString();
-        }
-        LOG.debug("findAllTest() retrieved:" + msg);
-
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end of findAllTest()");
-    }
-
-    @Test
-    public void deleteByIdTest() {
-        LOG.debug("start of deleteByIdTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end of deleteByIdTest()");
-    }
-
-    @Test
-    public void deleteTest() {
-        LOG.debug("start of deleteTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end ofdeleteTest()");
-    }
-
-    @Test
-    public void deleteAllTest() {
-        LOG.debug("start of deleteAllTest())");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end of deleteAllTest()");
-    }
-
-    @Test
-    public void findByRoleTest() {
-        LOG.debug("start of findByIdTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end of findByIdTest()");
-    }
-
-    @Test
-    public void findByNameTest() {
-        LOG.debug("start of findByNameTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end of findByNameTest())");
-
-    }
+//    private PersonDAO personDao = null;
+//
+//    private DAOFactoryJPAImpl daoFactory = new DAOFactoryJPAImpl();
+//
+//    @Before
+//    public void before() {
+//        personDao = daoFactory.getPersonDAO();
+//        assertNotNull(personDao);
+//    }
+//
+//    // initialises database for each test
+//    private void init() {
+//        // delete all in database
+//        personDao.deleteAll();
+//        // add 5 patients
+//        for (int i = 1; i < 6; i++) {
+//            Person p = new Person();
+//            p.setAddress("address_" + i);
+//            p.setFirstName("firstName_" + i);
+//            p.setSecondName("secondName_" + i);
+//            p.setRole(Role.PATIENT);
+//            personDao.save(p);
+//        }
+//    }
+//
+//    @Test
+//    public void createPersonDAOTest() {
+//        LOG.debug("start of createPersonDAOTest");
+//        // this test simply runs the before method
+//        LOG.debug("end of createPersonDAOTest");
+//    }
+//
+//    @Test
+//    public void findByIdTest() {
+//        LOG.debug("start of findByIdTest()");
+//        //TODO implement test
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end of findByIdTest()");
+//    }
+//
+//    @Test
+//    public void saveTest() {
+//        LOG.debug("start of saveTest()");
+//        //TODO implement test
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end of saveTest()");
+//    }
+//
+//    @Test
+//    public void findAllTest() {
+//        LOG.debug("start of findAllTest()");
+//
+//        init();
+//        List<Person> personList = personDao.findAll();
+//        assertNotNull(personList);
+//        
+//        // init should insert 5 people
+//        assertEquals(5, personList.size());
+//
+//        // print out result
+//        String msg = "";
+//        for (Person person : personList) {
+//            msg = msg +"\n   " +  person.toString();
+//        }
+//        LOG.debug("findAllTest() retrieved:" + msg);
+//
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end of findAllTest()");
+//    }
+//
+//    @Test
+//    public void deleteByIdTest() {
+//        LOG.debug("start of deleteByIdTest()");
+//        //TODO implement test
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end of deleteByIdTest()");
+//    }
+//
+//    @Test
+//    public void deleteTest() {
+//        LOG.debug("start of deleteTest()");
+//        //TODO implement test
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end ofdeleteTest()");
+//    }
+//
+//    @Test
+//    public void deleteAllTest() {
+//        LOG.debug("start of deleteAllTest())");
+//        //TODO implement test
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end of deleteAllTest()");
+//    }
+//
+//    @Test
+//    public void findByRoleTest() {
+//        LOG.debug("start of findByIdTest()");
+//        //TODO implement test
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end of findByIdTest()");
+//    }
+//
+//    @Test
+//    public void findByNameTest() {
+//        LOG.debug("start of findByNameTest()");
+//        //TODO implement test
+//        LOG.debug("NOT IMPLEMENTED");
+//        LOG.debug("end of findByNameTest())");
+//
+//    }
 }
