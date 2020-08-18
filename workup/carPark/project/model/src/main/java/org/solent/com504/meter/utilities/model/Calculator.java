fe591bf652295053527c424df3734dfcac07c649
Craig Gallen <cgallen@opennms.org>
2020-08-18 14:42:24 +0100

    added calculator - partly working

---
+++ workup/carPark/project/model/src/main/java/org/solent/com504/meter/utilities/model/Calculator.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.solent.com504.meter.utilities.model;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Iterator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.solent.com504.meter.model.dto.ChargeBand;
+import org.solent.com504.meter.model.dto.DailyChargingScheme;
+import org.solent.com504.meter.model.dto.DayOfWeek;
+
+
+/**
+ *
+ * @author cgallen
+ */
+public class Calculator {
+
+    final static Logger LOG = LogManager.getLogger(Calculator.class);
+
+    // Map<DayOfWeek,Map<Time in mins ,Double price per minute>>
+    private Map<DayOfWeek, SortedMap<Integer, Double>> weekTimeMap = new HashMap();
+
+    public Calculator(List<DailyChargingScheme> dailyChargingSchemeList) {
+
+        // fill weekTimeMap with empty days as default
+        for (DayOfWeek day : DayOfWeek.values()) {
+            TreeMap<Integer, Double> dailyChargeMap = new TreeMap();
+            dailyChargeMap.put(0, 0.0);
+            weekTimeMap.put(day, dailyChargeMap);
+        }
+
+        // fill weekTimeMap with actual values if set
+        if (dailyChargingSchemeList == null) {
+            throw new IllegalArgumentException("dailyChargingSchemeList cannot be null");
+        }
+        
+        for (DailyChargingScheme dailyScheme : dailyChargingSchemeList) {
+            DayOfWeek day = dailyScheme.getDay();
+            if (day == null) {
+                throw new IllegalArgumentException("day cannot be null in DailyChargingScheme");
+            }
+            TreeMap<Integer, Double> dailyChargeMap = new TreeMap();
+            List<ChargeBand> chargeBandList = dailyScheme.getChargeBandList();
+            for (ChargeBand chargeBand : chargeBandList) {
+                if (chargeBand.getHrs() == null || chargeBand.getMins() == null || chargeBand.getPricePerHr() == null) {
+                    throw new IllegalArgumentException("cannot have null value in chargeBand: " + chargeBand);
+                }
+                Double pricePerMinute = chargeBand.getPricePerHr() / 60;
+                dailyChargeMap.put(chargeBand.getHrs() * 60 + chargeBand.getMins(), pricePerMinute);
+            }
+            weekTimeMap.put(day, dailyChargeMap);
+        }
+    }
+
+    public Double getMoneyForMinutes(Date startTime, Integer parkingMinutes) {
+        Double charge = 0.0;
+        long startTimeLong = startTime.getTime();
+        
+        for (long min = 0; min <= parkingMinutes; min++ ){
+            long t = startTimeLong + min * 60000; // 60000ms = 1 minute
+            Date date = new Date(t);
+            DayOfWeek day = getDayOfWeekFromDate(date);
+            int minuteInDay = getMinuteInDayFromDate(date);
+            charge = charge + getPricePerMinute(day, minuteInDay);
+        }
+        return charge;
+    }
+
+    public Integer getMinutesForMoney(Date startTime, Double parkingPayment) {
+        Integer parkingMinutes = 0;
+        Double currentCharge = 0.0;
+        long startTimeLong = startTime.getTime();
+        
+        while( parkingPayment > currentCharge){
+            parkingMinutes++;
+            long t = startTimeLong + parkingMinutes * 60000; // 60000ms = 1 minute
+            Date date = new Date(t);
+            DayOfWeek day = getDayOfWeekFromDate(date);
+            int minuteInDay = getMinuteInDayFromDate(date);
+            currentCharge = currentCharge + getPricePerMinute(day, minuteInDay);
+        }
+        return parkingMinutes;
+    }
+
+    public double getPricePerMinute(DayOfWeek day, Integer minuteInDay) {
+        SortedMap<Integer, Double> dailyChargeMap = weekTimeMap.get(day);
+        return getPricePerMinute(dailyChargeMap, minuteInDay);
+    }
+
+    private double getPricePerMinute(SortedMap<Integer, Double> dailyChargeMap, Integer minuteInDay) {
+        Iterator<Integer> chargingSchemeIterator = dailyChargeMap.keySet().iterator();
+        Integer tn0 = chargingSchemeIterator.next();
+        Double aN = dailyChargeMap.get(tn0);
+        Integer tn1 = tn0;
+        while (chargingSchemeIterator.hasNext() && tn1 <= minuteInDay) {
+            tn0 = tn1;
+            tn1 = chargingSchemeIterator.next();
+            aN =  dailyChargeMap.get(tn0);
+        }
+        return aN;
+    }
+
+    public static int getMinuteInDayFromDate(Date date) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int hours = cal.get(Calendar.HOUR_OF_DAY);
+        int mins = cal.get(Calendar.MINUTE);
+
+        int ninuteInDay = hours * 60 + mins;
+
+        return ninuteInDay;
+
+    }
+
+    /**
+     * number ranges from 1 (Sunday) to 7 (Saturday)
+     * @param date
+     * @return
+     */
+    public static int getDayNumberFromDate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.DAY_OF_WEEK);
+    }
+
+    public static DayOfWeek getDayOfWeekFromDate(Date date) {
+        int dayNum = getDayNumberFromDate(date);
+
+        switch (dayNum) {
+            case 1:
+                return DayOfWeek.SUNDAY;
+            case 2:
+                return DayOfWeek.MONDAY;
+            case 3:
+                return DayOfWeek.TUESDAY;
+            case 4:
+                return DayOfWeek.WEDNESDAY;
+            case 5:
+                return DayOfWeek.THURSDAY;
+            case 6:
+                return DayOfWeek.FRIDAY;
+            case 7:
+                return DayOfWeek.SATURDAY;
+            default:
+                throw new IllegalArgumentException("illegal day number");
+        }
+
+    }
+
+}
---
+++ workup/carPark/project/model/src/test/java/org/solent/com504/meter/utilities/model/test/CalculatorTest.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.solent.com504.meter.utilities.model.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.solent.com504.meter.model.dto.ChargeBand;
+import org.solent.com504.meter.model.dto.DailyChargingScheme;
+import org.solent.com504.meter.model.dto.DayOfWeek;
+import org.solent.com504.meter.utilities.model.Calculator;
+
+/**
+ * @author cgallen
+ */
+public class CalculatorTest {
+
+    final static Logger LOG = LogManager.getLogger(CalculatorTest.class);
+
+    List<DailyChargingScheme> dailyChargingSchemeList = new ArrayList<DailyChargingScheme>();
+
+    @Before
+    public void createModel() {
+
+        List<DayOfWeek> weekDays = Arrays.asList(DayOfWeek.MONDAY,
+                DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY);
+
+        for (DayOfWeek day : weekDays) {
+            DailyChargingScheme dailyChargingScheme1 = new DailyChargingScheme();
+            dailyChargingScheme1.setDay(day);
+            List<ChargeBand> chargeBandList1 = new ArrayList<ChargeBand>();
+
+            // 0:00 to 09:00 charge 0
+            ChargeBand band1_1 = new ChargeBand();
+            band1_1.setHrs(0);
+            band1_1.setMins(0);
+            band1_1.setPricePerHr(0.0);
+            chargeBandList1.add(band1_1);
+
+            // 09:00 to 12:00 charge 2.00
+            ChargeBand band1_2 = new ChargeBand();
+            band1_2.setHrs(9);
+            band1_2.setMins(0);
+            band1_2.setPricePerHr(2.0);
+            chargeBandList1.add(band1_2);
+
+            // 12:00 to 18:30 charge 1.00
+            ChargeBand band1_3 = new ChargeBand();
+            band1_3.setHrs(12);
+            band1_3.setMins(0);
+            band1_3.setPricePerHr(1.0);
+            chargeBandList1.add(band1_3);
+
+            // 18:30 to 00:00 charge 0.50
+            ChargeBand band1_4 = new ChargeBand();
+            band1_4.setHrs(18);
+            band1_4.setMins(30);
+            band1_4.setPricePerHr(0.5);
+            chargeBandList1.add(band1_4);
+
+            dailyChargingScheme1.setChargeBandList(chargeBandList1);
+
+            dailyChargingSchemeList.add(dailyChargingScheme1);
+        }
+
+        // free at weekends
+        List<DayOfWeek> weekEndDays = Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+        for (DayOfWeek day : weekEndDays) {
+            DailyChargingScheme dailyChargingScheme2 = new DailyChargingScheme();
+            dailyChargingScheme2.setDay(day);
+            List<ChargeBand> chargeBandList2 = new ArrayList<ChargeBand>();
+
+            // 0:00 to 09:00 charge 0
+            ChargeBand band1_1 = new ChargeBand();
+            band1_1.setHrs(0);
+            band1_1.setMins(0);
+            band1_1.setPricePerHr(0.0);
+            chargeBandList2.add(band1_1);
+
+            dailyChargingScheme2.setChargeBandList(chargeBandList2);
+
+            dailyChargingSchemeList.add(dailyChargingScheme2);
+        }
+        
+        LOG.debug("dailyChargingSchemeList:");
+        for(DailyChargingScheme dailyChargingScheme :dailyChargingSchemeList){
+            LOG.debug("    "+dailyChargingScheme);
+        }
+    }
+
+    @Test
+    public void testCalculatorStaticMethods() throws ParseException {
+
+        // 2020-08-17 14:59 MONDAY
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        Date startDate = format.parse("2020-08-17 13:55");
+
+        DayOfWeek dow = Calculator.getDayOfWeekFromDate(startDate);
+        assertTrue(DayOfWeek.MONDAY.equals(dow));
+
+        startDate = format.parse("2020-08-18 14:59");
+
+        dow = Calculator.getDayOfWeekFromDate(startDate);
+        assertTrue(DayOfWeek.TUESDAY.equals(dow));
+
+        Integer mins = Calculator.getMinuteInDayFromDate(startDate);
+        Integer expected = 14 * 60 + 59;
+        assertEquals(expected, mins);
+
+    }
+
+    @Test
+    public void testCalculatorPricePerHour() throws ParseException {
+
+        Calculator calculator = new Calculator(dailyChargingSchemeList);
+
+
+        double aN = calculator.getPricePerMinute(DayOfWeek.MONDAY, 0);
+        assertEquals(0.0, aN, 0);
+
+        // price per minute at 09.00 on monday
+        aN = calculator.getPricePerMinute(DayOfWeek.MONDAY, 9 * 60);
+        assertEquals("price per minute at 09.00 on monday ", 2.0 / 60, aN, 0);
+
+        // price per minute at 13.00 on tuesday
+        aN = calculator.getPricePerMinute(DayOfWeek.TUESDAY, 13 * 60);
+        assertEquals("price per minute at 13.00 on tuesday ", 1.0 / 60, aN, 0);
+
+        // price per minute at 12.30 on saturday
+        aN = calculator.getPricePerMinute(DayOfWeek.SATURDAY, 12 * 60 + 30);
+        assertEquals("price per minute at 12.30 on saturday ", 0.0, aN, 0);
+
+    }
+
+    @Test
+    public void testCalculatorCharging() throws ParseException {
+
+        Calculator calculator = new Calculator(dailyChargingSchemeList);
+
+        // 2020-08-17 07:00 MONDAY for 3 hours 09.00 - 12.00 = 2*3 = £6.00
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date startDate = format.parse("2020-08-17 07:00");
+        Integer parkingMinutes = 180;
+
+        double charge = calculator.getMoneyForMinutes(startDate, parkingMinutes);
+        assertEquals("2020-08-17 07:00 MONDAY for "+parkingMinutes+"minutes ", 2.00, charge, 0.05);
+        
+        
+        startDate = format.parse("2020-08-17 09:00");
+        parkingMinutes = 3*60;
+        charge = calculator.getMoneyForMinutes(startDate, parkingMinutes);
+        assertEquals("2020-08-17 09:00 MONDAY for "+parkingMinutes+" minutes ", 6, charge, 0.05);
+        
+        startDate = format.parse("2020-08-17 12:00");
+        parkingMinutes = 3*60+30;
+        charge = calculator.getMoneyForMinutes(startDate, parkingMinutes);
+        assertEquals("2020-08-17 12:00 MONDAY for "+parkingMinutes+" minutes ", 3.5, charge, 0.05);
+        
+        startDate = format.parse("2020-08-17 18:30");
+        parkingMinutes = 60;
+        charge = calculator.getMoneyForMinutes(startDate, parkingMinutes);
+        assertEquals("2020-08-17 18:30 MONDAY for "+parkingMinutes+" minutes ", 2.75, charge, 0.05);
+        
+        parkingMinutes = 24*60;
+        // 09.00-12.00 (£2 per hr) £6 + 12.00-18.30 (£1per hr) £6.5 + 18.30-0.00 (£0.5 per hr) 5.5*0.5 = £2.75
+        charge = calculator.getMoneyForMinutes(startDate, parkingMinutes);
+        assertEquals("2020-08-17 07:00 MONDAY for "+parkingMinutes+" minutes ", 15.25 , charge, 0.05);
+
+    }
+
+}
