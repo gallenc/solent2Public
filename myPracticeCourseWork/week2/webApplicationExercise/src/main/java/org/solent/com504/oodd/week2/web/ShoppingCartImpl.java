75ed76b31dfaeebf7971c64542a93d6f3ee3e68f
Gilson96 <grafael99@gmail.com>
2020-11-09 21:27:18 +0000

    commit of week 2

--- myPracticeCourseWork/week2/webApplicationExercise/src/main/java/org/solent/com504/oodd/week2/web/ShoppingCartImpl.java
+++ myPracticeCourseWork/week2/webApplicationExercise/src/main/java/org/solent/com504/oodd/week2/web/ShoppingCartImpl.java
@@ -36,7 +36,7 @@ public class ShoppingCartImpl implements ShoppingCart{
 
     @Override
     public void removeItemFromCart(String itemUuid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        itemMap.remove(itemUuid);
     }
 
     @Override
