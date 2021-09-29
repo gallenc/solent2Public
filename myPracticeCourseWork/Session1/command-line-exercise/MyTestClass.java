dea3522d5f3f7fd55c0e3f02adfda49f0f52ca59
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2021-09-29 10:37:04 +0100

    Completed Command Line

---
+++ myPracticeCourseWork/Session1/command-line-exercise/MyTestClass.java
@@ -0,0 +1,22 @@
+/*
+ * This is not javadoc - usually the licence goes here
+ */
+
+ /**
+  * This is a javadoc comment on the Main class
+  */
+
+  public class MyTestClass{
+      public static void main(String[] args){
+          //this is a local comment which doesn't go in javadoc
+          MyTestClass myTestClass = new MyTestClass();
+          myTestClass.writeAboutMe();
+      }
+
+      /**
+       * This is a javadoc comment on writeAboutMe
+       */
+      public void writeAboutMe(){
+          System.out.println("I am running the following java class: " +MyTestClass.class);
+      }
+  }
---
+++ myPracticeCourseWork/Session1/command-line-exercise/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"MyTestClass"},{"p":"<Unnamed>","l":"MyTestClassLog4j"}]
