2b1d28016b80e0bed4ecafc4f69fd2da2811b972
kpeacock <kvpeacock25@gmail.com>
2021-09-29 13:05:20 +0100

    'week1'

---
+++ myPracticeCourseWork/session1/maven-test-exercise/example1/src/test/java/org/kvpeacock/com504/ood/MyTestClassTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kvpeacock.com504.ood;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author pc
+ */
+public class MyTestClassTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        MyTestClassLog4j myTest = new MyTestClassLog4j();
+        myTest.writeAboutMe();  
+    }
+    @Test
+    public void shouldAnswerWithName() {
+        MyTestClassLog4j myTest = new MyTestClassLog4j();
+        String _result = myTest.talkAboutMe("test");
+        System.out.println(_result);
+        assertTrue("talking about test".equals(_result));
+    }
+    
+
+}
