248a4f2c1d597e753a903cb5a29ae5c78e558177
gallenc <cg02r@zepler.net>
2021-11-13 12:13:41 +0000

    adding session8

---
+++ workup/session8/project-template/shopping-cart-application/web/src/main/java/org/solent/com504/oodd/cart/spring/web/SpringBootJspApplication.java
@@ -0,0 +1,19 @@
+package org.solent.com504.oodd.cart.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = "org.solent.com504.oodd.cart.spring.web")
+public class SpringBootJspApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootJspApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJspApplication.class);
+    }
+}
